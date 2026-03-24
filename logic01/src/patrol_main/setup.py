adbf5d21fbab7125cb5f6380ca4e9cc137a2d380
CyCle03 <cherrybear03@naver.com>
2026-03-24 15:22:26 +0900

    네임스페이스(TB3_2) 지원 SLAM 최적화 및 래퍼 런칭(LUA 설정 포함) 파일 추가

--- logic01/src/patrol_main/setup.py
+++ logic01/src/patrol_main/setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
 import os
+from glob import glob
 
 package_name = 'patrol_main'
 
@@ -11,8 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), ['launch/patrol.launch.py']),
-        (os.path.join('share', package_name, 'config'), ['config/shelf_coords.yaml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
