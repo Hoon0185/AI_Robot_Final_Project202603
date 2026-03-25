ee9fe83f9ec10b9b42dde31ca222cef10aae1e27
soyeon <sjwkao2@gmail.com>
2026-03-25 15:33:45 +0900

    ver 0.0.002 ASY : 장애물 대기 후 우회 기동 및 제어 안정화 구현 - 장애물 감지 시 10초 대기 후 우회(회전) 로직 추가 - 상태 전환 시 즉각적인 회전 명령 발행으로 외부 제어 간섭(급발진) 차단 - 우회 기동 중(wait_counter < 0) 센서 재감지 방지를 위한 예외 처리 적용

--- src/logic2_pkg/logic2_pkg/obstacle_manager.py
+++ src/logic2_pkg/logic2_pkg/obstacle_manager.py
@@ -1,19 +1,25 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
+from rclpy.qos import QoSProfile, ReliabilityPolicy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 
 class ObstacleManager(Node):
   def __init__(self):
     super().__init__('obstacle_manager')
+    self.declare_parameter('obstacle_wait_time',10) # 장애물 대기 시간(ui 조정을 위함)
 
+    qos_profile = QoSProfile(
+      reliability=ReliabilityPolicy.BEST_EFFORT,
+      depth=10
+    )
     # ---- 라이다(센서) 구독 ----
     self.scan_sub = self.create_subscription(
       LaserScan,
       'scan',
       self.scan_callback,
-      10
+      qos_profile
       )
     # ---- 로봇 실시간 위치 구독 ----
     self.odom_sub = self.create_subscription(
@@ -25,13 +31,17 @@ class ObstacleManager(Node):
     # ---- 구동 신호 전송 ----
     self.cmd_vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
 
-    # ---- 타이머 설정(0.1초 기준) ----
-    self.timer = self.create_timer(0.1, self.timer_callback)
+    # ---- 타이머 설정 ----
+    timer_pub = 0.02
+    self.timer_second = 1/timer_pub # 1초당 타이머 콜백 횟수 계산
+    self.timer = self.create_timer(timer_pub, self.timer_callback)
 
     # ---- 초기 변수 설정 ----
     self.is_blocked = False # 길 막힘 체크
+    self.clear_count = 0 # 장애물이 확실히 사라졌는지 체크
     self.wait_counter = 0 # 대기시간 측정
-    self.safe_distance = 0.5 # 50cm 기준
+    self.safe_distance = 0.35 # 35cm 기준 (적정 거리 조정중)
+    self.wait_time_s = self.get_parameter('obstacle_wait_time').get_parameter_value().integer_value # 대기시간
 
     # ---- 현재 로봇 좌표 저장 ----
     self.current_x = 0.0
@@ -44,13 +54,20 @@ class ObstacleManager(Node):
     self.current_y = msg.pose.pose.position.y
 
   def scan_callback(self, msg):
-    front_ranges = msg.ranges[0:5] + msg.ranges[355:360] # 10도
-    valid_ranges = [r for r in front_ranges if r > 0.0]
+    front_ranges = msg.ranges[0:30] + msg.ranges[330:360] # 60도
+    valid_ranges = [r for r in front_ranges if 0.1 < r < 3.0] # 유효반경 ~ 3m
 
     if valid_ranges:
       min_distance = min(valid_ranges)
 
-      if min_distance < self.safe_distance:
+      if self.wait_counter < 0:
+        # check_dist = 0.20 # 우회 중 장애물 기준 거리 (20cm)
+        return # 우회 중에는 장애물 감지 무시(수정예정)
+      else:
+        check_dist = self.safe_distance # 기본 장애물 기준 거리
+
+      if min_distance < check_dist:
+        self.clear_count = 0 # 장애물 감지 -> clear_count 초기화
         if not self.is_blocked:
           ## ---- 장애물 좌표 기록 ----
           self.obstacle_x = self.current_x
@@ -58,26 +75,51 @@ class ObstacleManager(Node):
 
           self.get_logger().warn(f'장애물이 감지되었습니다! 거리: {min_distance:.2f}m')
           self.is_blocked = True
+          self.wait_counter = 0
           self.stop_robot() # 발견 -> 정지
-        else:
-          ## ---- 장애물이 사라졌을 때(수정 예정) ----
-          pass
+      else:
+        ## ---- 장애물이 사라졌을 때(수정 예정) ----
+        if self.is_blocked and self.wait_counter >= 0:
+          self.clear_count += 1  # 판단용 카운트
+          if self.clear_count > self.timer_second * 0.5: # 0.5초 이상 장애물 사라짐 -> 재주행
+            self.get_logger().info('장애물이 사라졌습니다. 재주행을 시작합니다.')
+            self.is_blocked = False
+            self.clear_count = 0
+            self.wait_counter = 0
 
   def timer_callback(self):
+    self.wait_time_s = self.get_parameter('obstacle_wait_time').get_parameter_value().integer_value # 대기시간 실시간 업데이트
+    max_count = int(self.wait_time_s * self.timer_second) # 1초
+
     if self.is_blocked:
       self.stop_robot()
-
       self.wait_counter += 1
 
-      if self.wait_counter % 10 == 0 : # 1초 주기 로그
-        seconds = self.wait_counter // 10
-        self.get_logger().info(f'로딩중... {seconds}s / 10s')
+      if self.wait_counter % self.timer_second == 0 : # 1초 주기 로그
+        seconds = self.wait_counter // self.timer_second
+        self.get_logger().info(f'대기중... {seconds}s / {self.wait_time_s}s')
 
-      if self.wait_counter >= 100:
-        self.get_logger().info('10초가 지났습니다. 경로를 재탐색합니다.')
+      if self.wait_counter >= max_count:
+        self.get_logger().info(f'{self.wait_time_s}초가 지났습니다. 우회 회전을 시작합니다.')
         self.is_blocked = False
-        self.wait_counter = 0
-        ## ---- 우회 로직(수정 예정) ----
+        self.wait_counter = -int(2 * self.timer_second) # 우회 로직 진입 (2초)
+
+        msg = Twist()
+        msg.linear.x = 0.0
+        msg.angular.z = 1.0
+        self.cmd_vel_pub.publish(msg)
+
+    ## ---- 우회 로직(수정 예정) ----
+    elif self.wait_counter < 0:
+      self.wait_counter += 1
+
+      msg = Twist()
+      msg.linear.x = 0.0
+      msg.angular.z = 1.0
+      self.cmd_vel_pub.publish(msg)
+
+      if self.wait_counter == 0:
+        self.get_logger().info('우회 회전 완료. 순찰을 재개합니다.')
 
   def stop_robot(self):
     msg = Twist()
